b5324e8d25b96b4f10ee9ce1c80c855d9c2ba2f5
call23re <11638403+call23re@users.noreply.github.com>
2022-10-06 15:02:27 -0500

    Removed unused theming

--- src/Components/Settings.lua
+++ src/Components/Settings.lua
@@ -3,7 +3,6 @@ local Plugin = script.Parent.Parent
 local Roact = require(Plugin.Packages.roact)
 local StudioComponents = require(Plugin.Packages.studiocomponents)
 
-local withTheme = StudioComponents.withTheme
 local Checkbox = StudioComponents.Checkbox
 local Dropdown = StudioComponents.Dropdown
 
@@ -41,36 +40,34 @@ function Settings:init()
 end
 
 function Settings:render()
-	return withTheme(function(theme)
-		return Roact.createElement("Frame", {
-			AnchorPoint = self.props.AnchorPoint,
-			BackgroundTransparency = 1,
-			Size = UDim2.new(1, 0, 0, 35),
-			Position = self.props.Position
-		}, {
-			Roact.createElement(Checkbox, {
-				Label = "Place at Camera",
-				Value = self.state.Camera,
-				LayoutOrder = 1,
-				OnActivated = self.onUpdateCamera
-			}),
-			Roact.createElement(Checkbox, {
-				Label = "Unlock children",
-				Value = self.state.Unlock,
-				LayoutOrder = 2,
-				OnActivated = self.onUpdateLock
-			}),
-			Roact.createElement(Dropdown, {
-				Items = {"R15", "R6"},
-				Item = self.state.Rig,
-				LayoutOrder = 3,
-				OnSelected = self.onUpdateRig
-			}),
-			ListLayout = Roact.createElement("UIListLayout", {
-				Padding = UDim.new(0, 5)
-			})
+	return Roact.createElement("Frame", {
+		AnchorPoint = self.props.AnchorPoint,
+		BackgroundTransparency = 1,
+		Size = UDim2.new(1, 0, 0, 35),
+		Position = self.props.Position
+	}, {
+		Roact.createElement(Checkbox, {
+			Label = "Place at Camera",
+			Value = self.state.Camera,
+			LayoutOrder = 1,
+			OnActivated = self.onUpdateCamera
+		}),
+		Roact.createElement(Checkbox, {
+			Label = "Unlock children",
+			Value = self.state.Unlock,
+			LayoutOrder = 2,
+			OnActivated = self.onUpdateLock
+		}),
+		Roact.createElement(Dropdown, {
+			Items = {"R15", "R6"},
+			Item = self.state.Rig,
+			LayoutOrder = 3,
+			OnSelected = self.onUpdateRig
+		}),
+		ListLayout = Roact.createElement("UIListLayout", {
+			Padding = UDim.new(0, 5)
 		})
-	end)
+	})
 end
 
 return Settings

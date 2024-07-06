671209c8f0a15cd30dd080c0d07840e985a95f19
call23re <11638403+call23re@users.noreply.github.com>
2024-07-06 17:26:49 -0500

    Added DetermineAssetType function

--- src/Util.lua
+++ src/Util.lua
@@ -1,3 +1,6 @@
+local MarketplaceService = game:GetService("MarketplaceService")
+local Players = game:GetService("Players")
+
 local function joinDictionaries(...)
 	local out = {}
 	for i = 1, select("#", ...) do
@@ -12,14 +15,14 @@ local function ReplaceNewlines(Text)
 	return Text and Text:gsub("\n", ",") or ""
 end
 
-local function ExtractIDs(Text)
-	local IDs = {}
+local function ExtractIds(Text)
+	local Ids = {}
 
-	for ID, _ in Text:gmatch('[^,]+') do
-		table.insert(IDs, tonumber(ID))
+	for Id, _ in Text:gmatch('[^,]+') do
+		table.insert(Ids, tonumber(Id))
 	end
 
-	return IDs
+	return Ids
 end
 
 local function Sanitize(Text)
@@ -28,9 +31,56 @@ local function Sanitize(Text)
 	return Text
 end
 
+export type AssetType = {
+	["OBJECT"]: boolean,
+	["BUNDLE"]: boolean,
+	["CHARACTER"]: boolean
+}
+
+local Cache = {}
+
+local function DetermineAssetType(Id): AssetType
+	if Cache[Id] then return Cache[Id] end
+
+	local options: AssetType = {
+		["OBJECT"] = false,
+		["BUNDLE"] = false,
+		["CHARACTER"] = false
+	}
+
+	local ok = pcall(function() 
+		return MarketplaceService:GetProductInfo(Id)
+	end)
+
+	if ok then
+		options["OBJECT"] = true
+	end
+
+	ok = pcall(function()
+		return MarketplaceService:GetProductInfo(Id, Enum.InfoType.Bundle)
+	end)
+
+	if ok then
+		options["BUNDLE"] = true
+	end
+
+	ok = pcall(function()
+		return Players:GetNameFromUserIdAsync(Id)
+	end)
+
+	if ok then
+		options["CHARACTER"] = true
+	end
+
+	Cache[Id] = options
+
+	return options
+end
+
 return {
 	joinDictionaries = joinDictionaries,
 	ReplaceNewlines = ReplaceNewlines,
-	ExtractIDs = ExtractIDs,
-	Sanitize = Sanitize
+	ExtractIds = ExtractIds,
+	Sanitize = Sanitize,
+	DetermineAssetType = DetermineAssetType
 }
